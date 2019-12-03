58f42611976e2c30a1a29746ffe719cef4690138
Shehanka <hcsperera@outlook.com>
2019-12-03 20:56:54 +0530

    api main update

--- api/main.go
+++ api/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "Hello world"}`))
+}
+
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", r))
 	fmt.Println("Backed is running")
 }
