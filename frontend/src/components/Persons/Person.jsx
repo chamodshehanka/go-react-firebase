d59353cbd64637d07fe0481101593af4a5018a28
Shehanka <hcsperera@outlook.com>
2019-12-09 21:31:21 +0530

    Person class

---
+++ frontend/src/components/Persons/Person.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Person = props => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} and I'm {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default Person;
--- frontend/src/components/nav-bar.jsx
+++ frontend/src/components/nav-bar.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Person from './Persons/Person';
 
 class NavBar extends Component {
-    state = {  }
-    render() { 
-        return ( <div></div> );
-    }
+  state = {
+    persons: [
+        {name: 'Chamod', age: 5}
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <Person name={this.state.persons.name} age={this.state.persons.age} />
+      </div>
+    );
+  }
 }
- 
+
 export default NavBar;
