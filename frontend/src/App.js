5ab993259175dc431e913217154777992d4b1a2d
Shehanka <hcsperera@outlook.com>
2019-12-10 13:15:08 +0530

    switch name handler impl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Landing from './components/Landing';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -4,17 +4,32 @@ import Person from './Person';
 class Landing extends Component {
   state = {
     persons: [
-      { name: 'Chamod', age: 10 },
-      { name: 'Shehanka', age: 70 },
-      { name: 'Perera', age: 50 }
+      { id: 1, name: 'Chamod', age: 10 },
+      { id: 2, name: 'Shehanka', age: 70 },
+      { id: 3, name: 'Perera', age: 50 }
     ]
   };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { id: 1, name: 'Cat', age: 10 },
+        { id: 2, name: 'Shehanka', age: 70 },
+        { id: 3, name: 'Perera', age: 50 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div>
         {this.state.persons.map(e => (
-          <Person name={e.name} age={e.age} />
+          <Person key={e.id} name={e.name} age={e.age} />
         ))}
+
+        <button className='btn btn-primary' onClick={this.switchNameHandler}>
+          Switch Name
+        </button>
       </div>
     );
   }
