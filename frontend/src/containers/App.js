98580624763045f204ae9f2f3d69666c14bb3003
Shehanka <hcsperera@outlook.com>
2019-12-25 18:50:30 +0530

    Container refactored

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import LandingPage from './components/landing-page/landing-page';
+import LandingPage from './../components/landing-page/landing-page';
 import Radium from 'radium';
 
 function App() {
