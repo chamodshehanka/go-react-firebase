fcf267fff0e74aa832f510b0a14472c192e95ccf
Shehanka <hcsperera@outlook.com>
2019-12-25 18:34:06 +0530

    Styled Component

--- frontend/src/components/lists-module/Person.jsx
+++ frontend/src/components/lists-module/Person.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const Person = props => {
+  const style = {
+    '@media(min-width: 500px)': {
+      width: '450px'
+    }
+  };
+
   return (
-    <div className='Person'>
+    <div className='Person' style={style}>
       <p onClick={props.click}>
         I'm {props.name} I'm studying {props.degree}.
       </p>
@@ -21,4 +28,4 @@ const Person = props => {
   );
 };
 
-export default Person;
+export default Radium(Person);
